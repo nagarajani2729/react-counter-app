158a08519260d6600a1ec97d9076f4d07fd2a3c4
angristan <angristan@pm.me>
2018-09-03 01:27:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Counter App
+
+This project was made following this awesome [React crash course](https://www.youtube.com/watch?v=Ke90Tje7VS0) by Mosh.
---
+++ src/components/counter.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+export default class Counter extends Component {
+  componentDidUpdate(prevProps, prevState) {
+    // Access to prevProps and prevState
+    console.log("Counter - DidUpdate");
+  }
+
+  componentWillUnmount() {
+    console.log("Counter - WillUnmount");
+  }
+
+  render() {
+    console.log("Counter - Rendered");
+    return (
+      <div>
+        {this.props.children}
+        <span className={this.getBadgeClasses()}>{this.formatValue()}</span>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatValue() {
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
+  }
+}
